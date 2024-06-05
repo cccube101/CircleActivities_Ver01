107d510e8d3d99c82eea4a17b89ae1eeab21d2d7
s101_cube <kss-22200032@nsgcl.jp>
2024-06-05 13:16:56 +0900

    制作用フォルダの作成、注意事項の追加

---
+++ Assets/03_CreateArea/Hashimoto/AudioManager_Hashimoto.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AudioManager_Hashimoto : MonoBehaviour
+{
+    // ---------------------------- SerializeField
+
+
+    // ---------------------------- Field
+
+
+
+    // ---------------------------- UnityMessage
+
+
+
+
+    // ---------------------------- PublicMethod
+
+
+
+
+
+    // ---------------------------- PrivateMethod
+
+
+}
---
+++ Assets/03_CreateArea/Hashimoto/UIController_Hashimoto.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class UIController_Hashimoto : MonoBehaviour
+{
+    // ---------------------------- SerializeField
+
+
+    // ---------------------------- Field
+
+
+
+    // ---------------------------- UnityMessage
+
+
+
+
+    // ---------------------------- PublicMethod
+
+
+
+
+
+    // ---------------------------- PrivateMethod
+
+
+}
